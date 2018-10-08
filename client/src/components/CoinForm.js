ad7e9ca4ac071bcc9e7406400014a1b79c02b4d0
Rachel Inkala <rachelinkala@gmail.com>
2018-10-08 11:21:00 -0600

    coin form

--- client/src/components/CoinForm.js
+++ client/src/components/CoinForm.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Form } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { addCoin } from '../reducers/coins'
 
 class CoinForm extends React.Component {
+  state = { coin: '' }
+
+  handleChange = (e) =>{
+    const { name, value } = e.target
+    this.setState({ [name]: value })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const { dispatch } = this.props
+    const { coin } = this.state
+    dispatch(addCoin(coin))
+    this.setState({ coin: '' })
+  }
+
   render() {
-    return null
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Input
+          label="Watch Coin"
+          value={this.state.coin}
+          onChange={this.handleChange}
+          name="coin"
+          required
+        />
+        <Form.Button>Add Coin</Form.Button>
+      </Form>
+    )
   }
 }
 
-export default CoinForm
+export default connect()(CoinForm)
